428568fc50cc464e44849d08ba915477959b3547
BhaktiRohane2404 <98884739+BhaktiRohane2404@users.noreply.github.com>
2022-02-15 22:24:13 -0800

    Add files via upload

---
+++ GamblingSimulator.java
@@ -0,0 +1,9 @@
+package com.gambling.simulator;
+
+public class GamblingSimulator {
+        public static void main(String[] args) {
+            System.out.println("Welcome to the Gambling simulation program.");
+        }
+
+
+}
